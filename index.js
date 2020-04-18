b37203f5ce0cc2dcec1973cb7c8b1758dc023ece
rauferdeveloper <rauletecolegio@gmail.com>
2020-04-18 15:11:50 +0200

    [add] Web scraping

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const config = require('./config/config.json')
 const { products, email, token , user_id, time} = config;
 const bot = new TelegramBot(token, {polling: true});
 
-
-const PURCHASE_URL = "https://www.decathlon.es/es/ajax/rest/model/atg/commerce/order/purchase/CartModifierActor/addItemToOrder";
+//API
+//const PURCHASE_URL = "https://www.decathlon.es/es/ajax/rest/model/atg/commerce/order/purchase/CartModifierActor/addItemToOrder";
 
 const transporter = nodemailer.createTransport({
 	    host: email.host,
@@ -112,43 +112,7 @@ async function init() {
 				
 			} 
 		});
-			
-		/*if (product.outOfStock) {
-			const params = new URLSearchParams();
-			params.append('catalogRefIds', product.catalogRefId);
-			params.append('productId', product.productId);
-			params.append('quantity', 1);
 
-			await fetch(PURCHASE_URL, { method: 'POST', body: params })
-	    .then(res => res.json())
-	    .then(async json => {
-	    	const isOnStock = json.responseTO.data;
-	    	if (isOnStock) {
-				
-	    		let info = await transporter.sendMail({
-				    from: email.from, // sender address
-				    to: email.to, // list of receivers
-				    subject: "YA HAY STOCK EN DECATHLON ✔", // Subject line
-				    text: `HAY UNIDADES DE ${product.name}`, // plain text body
-				    html: `<b>HAY UNIDADES DE ${product.name}</b> 
-				    <p> CORRE INSENSATO!! </p>
-				    <p>	LINK: <a href="${product.link}">${product.link}</a> </p>`
-				})
-				if(user_id > 0){
-					bot.sendMessage(user_id,'<b>HAY UNIDADES DE ' + product.name + '</b>\n CORRE INSENSATO!! \n LINK: <a href="' + product.link + '">' + product.link + '</a> ', {parse_mode: "HTML"});
-				}
-				product.outOfStock = false;
-				//console.log(moment().format(), `There is stock for -> ${product.name}. Email already sent`)
-			} else {
-				if(user_id > 0){
-					bot.sendMessage(user_id, 'Aún no hay stock de ' + product.name, {parse_mode: "HTML"});
-				}
-
-				//console.log(moment().format(), `No stock for -> ${product.name}.`)
-			}
-	    })
-	    .catch(err => console.log(err));
-		}*/
 	}
 }
 
